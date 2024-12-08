554577b4d2c86cd1b74d921f1c723e4b210b60a5
86152 <2405961518@qq.com>
2024-12-08 12:50:16 +0800

    init

---
+++ ugc/src/main/java/sgms/ugc/TommApplication.java
@@ -0,0 +1,13 @@
+package sgms.ugc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TommApplication.class, args);
+	}
+
+}
